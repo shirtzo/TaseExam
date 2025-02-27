0107ae901fa77d9c6fe089435230674a610b82ab
eden <your.email@example.com>
2025-02-27 14:38:47 +0000

    changes public files method

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; 
 
-app.use(express.static('public')); 
+app.use(express.static('public'));app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
